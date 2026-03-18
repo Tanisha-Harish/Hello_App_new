89f252368d6e3fd0404cd4efd9837e960c0da88e
Tanisha-Harish <tanishahari08@gmail.com>
2026-03-18 10:52:43 +0530

    Renaming HelloApp file Structure UC 2

--- Hello_App/src/main/java/org/example/HelloApp.java
+++ Hello_App/src/main/java/org/example/HelloApp.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class uc2_helloapp {
+public class HelloApp {
     public static void main(String[] args){
         String name = args[0];
         System.out.println("Hello, " + name + "!");
