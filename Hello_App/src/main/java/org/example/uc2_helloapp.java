0bc1117a433d7c92e616cdd74b5509a3bce42f67
Tanisha-Harish <tanishahari08@gmail.com>
2026-03-18 10:09:16 +0530

    Added uc-2 code

---
+++ Hello_App/src/main/java/org/example/uc2_helloapp.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class uc2_helloapp {
+    public static void main(String[] args){
+        String name = args[0];
+        System.out.println("Hello, " + name + "!");
+    }
+}
