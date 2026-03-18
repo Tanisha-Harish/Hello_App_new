b9210ef4cc04db9e22ed531477b2f1ce9f821f9b
Tanisha-Harish <tanishahari08@gmail.com>
2026-03-18 16:30:38 +0530

    Added to uc-5

--- Hello_App/src/main/java/org/example/HelloApp.java
+++ Hello_App/src/main/java/org/example/HelloApp.java
@@ -1,8 +1,21 @@
 package org.example;
 
 public class HelloApp {
-    public static void main(String[] args){
-        String name = args[0];
-        System.out.println("Hello, " + name + "!");
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+
+            StringBuilder sb = new StringBuilder();
+
+            for (String name : args) {
+                sb.append(name).append(", ");
+            }
+
+            sb.setLength(sb.length() - 2);
+
+            System.out.println("Hello, " + sb.toString() + "!");
+        }
     }
 }
